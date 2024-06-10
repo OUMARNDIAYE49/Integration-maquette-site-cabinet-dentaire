48885337ce404205356ee687bc3434cefa17bc17
OUMARNDIAYE49 <oumar.ndiaye4952@gmail.com>
2024-06-10 16:33:36 +0000

    ajustement

--- page.js
+++ page.js
@@ -1,25 +1,19 @@
 
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
 
-        document.getElementById('formulaire ').addEventListener('submit', function(event) {
-            const titre= document.getElementById('titre');
-            const  phone= document.getElementById('phone');
-            
-            
-
-            if (!titre.checkValidity()) {
-                event.preventDefault();
-                alert(titre.title);
-            }
-
-            if (!phone.checkValidity()) {
-                event.preventDefault();
-                alert(phone.title);
-            }
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
 
     
-        
 
-         
-            
-        });
     
+
+
+
+
+
